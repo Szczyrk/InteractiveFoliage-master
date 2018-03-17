28b7e551567dac2e5e00b500e79e80f962437224
Szczyrk <jakubszczyrk@wp.pl>
2018-03-17 16:34:23 +0100

    Kuba

---
+++ Assets/Article/Setup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setup : MonoBehaviour {
+
+
+	public Camera cam;
+
+    public Material mat;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Matrix4x4 vp = cam.projectionMatrix * cam.worldToCameraMatrix;
+
+        mat.SetMatrix("_Matrix", vp);
+        mat.SetTexture("_PlayersTexture", cam.targetTexture);
+
+    }
+}
---
+++ Assets/Zadanie1/Setup2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setup2 : MonoBehaviour {
+
+
+	public Camera cam;
+    public Camera cam2;
+    public Material mat;
+
+	// Use this for initialization
+	void Start () {
+		Matrix4x4 vp = cam.projectionMatrix * cam.worldToCameraMatrix;
+        Matrix4x4 vp2 = cam2.projectionMatrix * cam2.worldToCameraMatrix;
+
+        mat.SetMatrix("_Matrix2", vp2);
+        mat.SetMatrix("_Matrix", vp);
+		mat.SetTexture("_PlayersTexture", cam.targetTexture);
+        mat.SetTexture("_PlayersTexture2", cam2.targetTexture);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
